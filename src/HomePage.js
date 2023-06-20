83c73e67c0288e29b3aa69eda9d42ff8c8738688
Kishan Kumar <kishan.kumar@zimtive.com>
2023-06-20 15:14:57 +0530

    final push

--- src/HomePage.js
+++ src/HomePage.js
@@ -5,41 +5,64 @@ import Message from './Message';
 function HomePage() {
     const [msg, setMsg] = useState("");
     const [msgList, setMsgList] = useState([]);
+    const [editMsg, setEditMsg] = useState("");
 
     function handleSubmit() {
-        if(msg){
-        setMsgList([...msgList,{msg: msg, isDeleted: false}])
-        let data = JSON.stringify([...msgList, {msg: msg, isDeleted: false}])
-        localStorage.setItem("message",data)
-        setMsg("")
+        if (msg) {
+            setMsgList([...msgList, { msg: msg, isDeleted: false }])
+            let data = JSON.stringify([...msgList, { msg: msg, isDeleted: false }])
+            localStorage.setItem("message", data)
+            setMsg("")
         }
     }
-    function checkKey(e){
-        if(e.key === 'Enter')
-            handleSubmit();
-    }
-    useEffect(()=>{
+    function handleEditChange() {
+        if(!msg){
+            setEditMsg("");
+            return;
+        }
         let tempStr = localStorage.getItem("message");
-        if(tempStr){
         let tempArr = JSON.parse(tempStr)
-        tempArr && setMsgList(tempArr)
+        
+        tempArr.forEach(element => {
+            if(element.msg === editMsg){
+                element.msg = msg;
+            }
+        });
+        
+
+        let data = JSON.stringify(tempArr);
+        localStorage.setItem("message", data);
+        setMsgList(tempArr)
+        setMsg("")
+        setEditMsg("")
+    }
+
+    function checkKey(e) {
+        if (e.key === 'Enter')
+            editMsg? handleEditChange() : handleSubmit();
+    }
+    useEffect(() => {
+        let tempStr = localStorage.getItem("message");
+        if (tempStr) {
+            let tempArr = JSON.parse(tempStr)
+            tempArr && setMsgList(tempArr)
         }
-    },[])
+    }, [])
     return (
         <div className='bg-red-400 h-screen w-screen flex items-center justify-center '>
             <div className='h-[70%] w-[60%] shadow-2xl'>
                 <div className='h-[10%] flex items-center justify-center space-x-3 bg-red-700 rounded-t-lg'>
-                    <input type='text' value={msg} placeholder='Enter your text' onChange={(e)=>setMsg(e.target.value)} className='h-[5vh] w-[40%] rounded-xl px-2 outline-none' onKeyPress={checkKey} />
+                    <input type='text' value={msg} placeholder='Enter your text' onChange={(e) => setMsg(e.target.value)} className='h-[5vh] w-[40%] rounded-xl px-2 outline-none' onKeyPress={checkKey} />
                     <div className='flex items-center justify-center w-fit h-full'>
-                        <div className='bg-green-700 p-1 rounded-2xl cursor-pointer' onClick={handleSubmit}>
+                        <div className='bg-green-700 p-1 rounded-2xl cursor-pointer' onClick={editMsg ? handleEditChange : handleSubmit}>
                             <Plus height="20px" fill="white" />
                         </div>
                     </div>
                 </div>
                 <div className='h-[90%] px-4 py-2 bg-white rounded-b-lg space-y-2 overflow-scroll scrollbar-hide'>
-                   { msgList.map((inp,ind)=>{
-                        return <Message key={ind} message={inp} setMsgList={setMsgList} />
-                    }) }
+                    {msgList.map((inp, ind) => {
+                        return <Message key={ind} message={inp} setMsgList={setMsgList} setMsg={setMsg} setEditMsg={setEditMsg} />
+                    })}
                 </div>
             </div>
         </div>
--- src/Message.js
+++ src/Message.js
@@ -3,40 +3,32 @@ import Delete from './Delete';
 import Edit from './Edit';
 
 function Message(props) {
-    // const [checked, setChecked] = useState(false)
-
     function handleChange() {
-        // setChecked(!checked)
-
         let temp = localStorage.getItem("message")
-
         let array = JSON.parse(temp)
-
         array.forEach(element => {
             if (element.msg === props.message.msg) {
                 element.isDeleted = !element.isDeleted;
                 props.message.isDeleted = !props.message.isDeleted
             }
         });
-
         let string = JSON.stringify([...array])
-
         localStorage.setItem("message", string)
         props.setMsgList(array);
+    }
 
+    function handleEdit(){
+        props.setMsg(props.message.msg);
+        props.setEditMsg(props.message.msg);
     }
 
     function handleDelete() {
         let temp = localStorage.getItem("message")
-
         let array = JSON.parse(temp)
-
         array = array.filter((e) => {
             return e.msg !== props.message.msg
         })
-
         let string = JSON.stringify([...array])
-
         localStorage.setItem("message", string)
         props.setMsgList(array);
     }
@@ -48,7 +40,7 @@ function Message(props) {
                 {props.message.msg}
             </div>
             <div className='flex items-center justify-center w-fit h-full'>
-                <div className='bg-blue-700 p-1 rounded-2xl cursor-pointer'>
+                <div className='bg-blue-700 p-1 rounded-2xl cursor-pointer' onClick={handleEdit}>
                     <Edit height="20px" fill="white" />
                 </div>
             </div>
